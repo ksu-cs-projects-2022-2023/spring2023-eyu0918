ac1fa92efef782ac592de040c69979ba098792ae
Eric Yu <eyu0918@gmail.com>
2023-03-01 14:19:35 -0600

    Fixed the issue where react would try to render before the data comes in from the back-end.

--- src/components/pages/Members.js
+++ src/components/pages/Members.js
@@ -1,41 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../App.css";
 import Footer from "../Footer";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 import { useReadCypher } from "use-neo4j/dist/cypher";
 
 function Members() {
-  const query = `MATCH (p:Person) RETURN p`;
+  const [schoolYear, setSchoolYear] = useState("");
 
-  const { loading, first } = useReadCypher(query);
-  if (loading) return <div>Loading...</div>;
+  const query = `MATCH (p:Person ) RETURN p`;
+  const { first } = useReadCypher(query);
 
-  const value = first.get("p");
+  const handleChange = (event) => {
+    setSchoolYear(event.target.value);
+  };
 
-  return (
-    <>
-      <h1 className="members">MEMBERS</h1>
-      <div className="members-container">
-        <img src="images/2022-2023/PEDRO GARCIA.jpg" alt="Pedro Garcia" />
-        <div className="centered-text">
-          <p>
-            Pedro Garcia <br />
-            Architecture <br />
-            Freshman <br />
-            *NO LONGER IN THE HOUSE* <br />
-          </p>
-          <a
-            href="https://www.smithscholarshiphouse.org/current-smithees"
-            target="_blank"
-            rel="noreferrer"
-          >
-            LinkedIn
-          </a>
+  let value;
+
+  if (first === undefined) {
+    return <div>LOADING...</div>;
+  } else {
+    value = first.get("p");
+
+    return (
+      <>
+        <h1 className="members">MEMBERS</h1>
+        <div className="members-input-container">
+          <FormControl fullWidth>
+            <InputLabel id="year-select-label">School Year</InputLabel>
+            <Select
+              labelId="year-select-label"
+              value={schoolYear}
+              label="School Year"
+              onChange={handleChange}
+            >
+              <MenuItem value={"2022-2023"}>2022-2023</MenuItem>
+              <MenuItem value={"2023-2024"}>2023-2024</MenuItem>
+              <MenuItem value={"2024-2025"}>2024-2025</MenuItem>
+            </Select>
+          </FormControl>
         </div>
-      </div>
-      <div>{value.properties.firstName}</div>
-      <Footer />
-    </>
-  );
+        <div className="members-container">
+          <img
+            src={value.properties.imageSource}
+            alt={value.properties.fullName}
+          />
+          <div className="centered-text">
+            <p>
+              {value.properties.fullName} <br />
+              {value.properties.major} <br />
+              Freshman <br />
+              {value.properties.position} <br />
+            </p>
+            <a
+              href={value.properties.linkedIn}
+              target="_blank"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+          </div>
+        </div>
+        <div>{schoolYear}</div>
+        <div>{value.properties.firstName}</div>
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default Members;
